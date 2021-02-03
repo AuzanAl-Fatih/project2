dd42016734b0fb1747ae9c94e4b29013e9b11de0
Ahmad Auzan Varian Syahputra <ahmad.14117007@student.itera.ac.id>
2021-02-03 22:32:34 +0700

    Navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const handlerClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(!false);
+  const [button, setButton] = useState(true);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3"></i>
+            Bocil Kematian <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handlerClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
@@ -49,6 +60,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
